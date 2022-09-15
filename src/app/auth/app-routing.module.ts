937c861f0ff86a730b104daab7764b922a5f5abe
arun raju <arun@nintrivalabs.com>
2022-09-15 09:21:38 +0530

    set login page as default route

--- src/app/auth/app-routing.module.ts
+++ src/app/auth/app-routing.module.ts
@@ -1,25 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Route } from '@angular/router';
 
-
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 
-
 const routes: Route[] = [
-    {
-        path: 'login',
-        component: LoginComponent
-    },
-    {
-        path: 'signup',
-        component: SignupComponent
-    }
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  { path: '**', redirectTo: 'login', pathMatch: 'full' },
 ];
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes)
-    ],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/auth/authLayout/authlayout.component.ts
+++ src/app/auth/authLayout/authlayout.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-authlayout',
-    template: `<div class="container">
-                    <mat-card>
-                    <ng-content></ng-content>
-                    </mat-card>
-               </div>`,
+    templateUrl:'./authlayout.component.html',
     styleUrls:['./authlayout.component.scss']
 })
 export class AuthLayoutComponent {
